c5f3f1954e7acd5012f47badf44c627b3392aa7e
Vishesh <guptavishesh2@gmail.com>
2025-05-10 16:39:44 +0530

    Removed the user from log

--- components/nav.tsx
+++ components/nav.tsx
@@ -56,8 +56,6 @@ export function Nav({ user, profileFullName, profilePictureUrl }: NavProps) {
     return null;
   }
 
-  console.log("user", user)
-
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 px-4">
       <div className="flex h-16 items-center">
