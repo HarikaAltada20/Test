54ca3272de47908290fb5485487607169fa24a66
Vishesh <guptavishesh2@gmail.com>
2025-07-23 12:26:47 +0530

    Added google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { createClient } from "@/utils/supabase/server";
 import { ConditionalFooter } from "./conditional-footer";
 import { Analytics } from "@vercel/analytics/next";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -124,6 +125,20 @@ export default async function RootLayout({
         </div>
         <Toaster />
         <Analytics />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-8J6VZKVWLF"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-8J6VZKVWLF', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
       </body>
     </html>
   );
