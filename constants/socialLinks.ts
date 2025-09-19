56fdb7041494bd9cb04fadba053d7c86377a926a
Vishesh <guptavishesh2@gmail.com>
2025-09-19 20:46:50 +0530

    added Reel reapers

--- constants/socialLinks.ts
+++ constants/socialLinks.ts
@@ -2,7 +2,8 @@ export const SOCIAL_LINKS = {
   instagram: "https://www.instagram.com/try_gameofcreators/",
   youtube: "https://www.youtube.com/@gameofcreators",
   twitter: "https://x.com/gameofcreators",
-  discord: "https://discord.com/invite/55hksuB2c7",
+  // discord: "https://discord.com/invite/55hksuB2c7", ( GOC server)
+  discord: "https://discord.gg/5GUZNAYwgu", // Reel Reapers
   linkedin: "https://www.linkedin.com/company/game-of-creators",
 } as const;
 
