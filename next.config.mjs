a27396b9cf71f166257e52b12cdf29fe317afa10
HarikaAltada <altadaharika55@gmail.com>
2026-08-05 16:35:07 +0530

    chore: remove unused outputFileTracingIncludes and clean up package dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -25,11 +25,6 @@ const nextConfig = {
     staticGenerationRetryCount: 3,
   },
   outputFileTracingRoot: path.join(__dirname),
-  outputFileTracingIncludes: {
-    '/api/admin/download-reel': ['./bin/**/*'],
-    '/api/admin/bulk-download': ['./bin/**/*'],
-    '/api/**/*': ['./bin/**/*'],
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
